4a093a34c690f8fb50e7c0c12b2c0ff5c0c5bcd0
Boyan Venkov <boyanvenkov@yahoo.com>
2025-10-27 19:28:28 +0200

    Add project files.

---
+++ TestApp.UnitTests/PalindromeTests.cs
@@ -0,0 +1,74 @@
+﻿using NUnit.Framework;
+
+using System.Collections.Generic;
+
+namespace TestApp.UnitTests;
+
+public class PalindromeTests
+{
+    [Test]
+    public void Test_IsPalindrome_ValidPalindrome_ReturnsTrue()
+    {
+        // Arrange
+        List<string> input = new List<string> { "level" };
+        bool expected = true;
+        // Act
+        bool result = Palindrome.IsPalindrome(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_IsPalindrome_EmptyList_ReturnsTrue()
+    {
+        // Arrange
+        List<string> input = new();
+
+        // Act
+        bool result = Palindrome.IsPalindrome(input);
+
+        // Assert
+        Assert.That(result, Is.True);
+    }
+
+    [Test]
+    public void Test_IsPalindrome_SingleWord_ReturnsTrue()
+    {
+        // Arrange
+        List<string> input = new() { "asdsa"};
+
+        //Act
+        bool result = Palindrome.IsPalindrome(input);
+
+        // Assert
+        Assert.That(result, Is.True);
+
+    }
+
+    [Test]
+    public void Test_IsPalindrome_NonPalindrome_ReturnsFalse()
+    {
+        // Arrange
+        List<string> input = new() { "Hello" };
+
+        // Act
+        bool result = Palindrome.IsPalindrome(input);
+
+        // Assert
+        Assert.That(result, Is.False);
+    }
+
+    [Test]
+    public void Test_IsPalindrome_MixedCasePalindrome_ReturnsTrue()
+    {
+        // Arrange
+        List<string> input = new() { "LeVeL"};
+
+        // Act
+        bool result = Palindrome.IsPalindrome(input);
+
+        // Assert
+        Assert.That(result, Is.True);
+    }
+}
