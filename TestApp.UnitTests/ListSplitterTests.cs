6d8422650e145bd3465cb07f6565197b32e39e71
Boyan Venkov <boyanvenkov@yahoo.com>
2025-10-28 18:08:37 +0200

    Added an Upgrade

--- TestApp.UnitTests/ListSplitterTests.cs
+++ TestApp.UnitTests/ListSplitterTests.cs
@@ -12,24 +12,63 @@ public class ListSplitterTests
     [Test]
     public void Test_SplitEvenAndOdd_EmptyListParameter_ReturnsEmptyEvenAndOddLists()
     {
+        // Arrange
+        List<int> input = new List<int>();
+
+
+        // Act
+        (List<int> evens, List<int> odds) result = ListSplitter.SplitEvenAndOdd(input);
+
+        // Assert
+        Assert.That(result.evens, Is.Empty);
+        Assert.That(result.odds, Is.Empty);
 
     }
 
     [Test]
     public void Test_SplitEvenAndOdd_OnlyEvenValues_ReturnsEmptyOddList()
     {
+        // Arrange
+        List<int> input = new List<int>() { 2, 4, 6, 98, 76 };
 
+
+        // Act
+        (List<int> evens, List<int> odds) result = ListSplitter.SplitEvenAndOdd(input);
+
+        // Assert
+        Assert.That(result.evens, Is.EqualTo(input));
+        Assert.That(result.odds, Is.Empty);
     }
 
     [Test]
     public void Test_SplitEvenAndOdd_OnlyOddValues_ReturnsEmptyEvenList()
     {
+        // Arrange
+        List<int> input = new List<int>() { 3, 5, 7, 99, 101 };
 
+
+        // Act
+        (List<int> evens, List<int> odds) result = ListSplitter.SplitEvenAndOdd(input);
+
+        // Assert
+        Assert.That(result.evens, Is.Empty);
+        Assert.That(result.odds, Is.EqualTo(input));
     }
 
     [Test]
     public void Test_SplitEvenAndOdd_EvenAndOddValues_ReturnsListWithCorrectValues()
     {
+        // Arrange
+        List<int> input = new List<int>() { 2, 4, 7, 6, 15, 98, 76 };
+        List<int> evenResult = new List<int>() { 2, 4, 6, 98, 76 };
+        List<int> oddsResult = new List<int>() { 7, 15 };
 
+
+        // Act
+        (List<int> evens, List<int> odds) result = ListSplitter.SplitEvenAndOdd(input);
+
+        // Assert
+        Assert.That(result.evens, Is.EqualTo(evenResult));
+        Assert.That(result.odds, Is.EqualTo(oddsResult));
     }
 }
