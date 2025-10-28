566a2d2776a3686be4f75ebe8e9deaf0cdcf66b0
Boyan Venkov <boyanvenkov@yahoo.com>
2025-10-28 17:38:44 +0200

    Added an Upgrade

--- TestApp.UnitTests/ListRemover_RemoveGreaterThanTests.cs
+++ TestApp.UnitTests/ListRemover_RemoveGreaterThanTests.cs
@@ -12,24 +12,58 @@ public class ListRemover_RemoveGreaterThanTests
     [Test]
     public void Test_RemoveElementsGreaterThan_EmptyListParameter_ReturnsEmtyList()
     {
+        // Arrange
+        List<int> input = new List<int>();
+        int treshhold = 7;
+
+        // Act
+        List<int> result = ListRemover.RemoveElementsGreaterThan(input, treshhold);
+
+        // Assert
+        Assert.That(result, Is.Empty);
 
     }
 
     [Test]
     public void Test_RemoveElementsGreaterThan_ListWithGreaterThanThresholdElements_ReturnsEmtyList()
     {
+        // Arrange
+        List<int> input = new List<int>() { 7, 8, 9, 10, 11};
+        int treshhold = 6;
 
+        // Act
+        List<int> result = ListRemover.RemoveElementsGreaterThan(input, treshhold);
+
+        // Assert
+        Assert.That(result, Is.Empty);
     }
 
     [Test]
     public void Test_RemoveElementsGreaterThan_ListWithLessThanOrEqualToThresholdElements_ReturnsSameList()
     {
+        // Arrange
+        List<int> input = new List<int>() { 7, 8, 9, 10, 11 };
+        int treshhold = 11;
 
+        // Act
+        List<int> result = ListRemover.RemoveElementsGreaterThan(input, treshhold);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(input));
     }
 
     [Test]
     public void Test_RemoveElementsGreaterThan_ListWithLessThanEqualAndGreaterThanThresholdElements_ReturnsOnlyLessThanOrEqualToThreshold()
     {
+        // Arrange
+        List<int> input = new List<int>() { 7, 8, 9, 10, 11, 35 };
+        int treshhold = 10;
+        List<int> expected = new List<int>() { 7, 8, 9, 10 };
 
+        // Act
+        List<int> result = ListRemover.RemoveElementsGreaterThan(input, treshhold);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
     }
 }
